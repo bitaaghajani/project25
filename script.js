93f57b4da90de7e2d4dc52ab175f79a7df336d16
bitaaghajani <biita.aghajanii@gmail.com>
2022-06-11 20:02:34 +0430

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const nav=document.querySelector('.nav');
+window.addEventListener('scroll',getNavFixed);
+
+
+function getNavFixed(){
+    console.log(window.scrollY);
+    if(window.scrollY > nav.offsetHeight +150){
+        nav.classList.add('active');
+    }
+    else{
+        nav.classList.remove('active');
+    }
+}
